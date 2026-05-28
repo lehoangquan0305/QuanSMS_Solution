2cfea80fb68b276b76b12154132f1e37311ac520
Le Hoang Quan <lehoangquan0305@example.com>
2026-05-28 14:24:18 +0700

    sữa bài Buoi2

---
+++ CMS.Backend/Controllers/OrderDetailController.cs
@@ -0,0 +1,29 @@
+﻿using CMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Backend.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderDetailController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // =========================
+        // DANH SÁCH CHI TIẾT ĐƠN
+        // =========================
+        public IActionResult Index()
+        {
+            var details = _context.OrderDetails
+                .Include(od => od.Order)
+                .Include(od => od.Product)
+                .ToList();
+
+            return View(details);
+        }
+    }
+}
